bd4ff26865219d5bdc3f0952a5aa1c0963f0865f
MNG <my.ni.jp@gmail.com>
2016-06-04 15:30:15 +0200

    deuxieme modification console

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
 # Create your models here.
-from django.utils import timezone # Permet d'importer des morceaux d'autres fichiers
-
+from django.utils import timezone 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User') # Lien vers un autre modèle
-    title = models.CharField(max_length=200) # Définit un champ de texte avec ici un nombre max de caractères (200)
-    text = models.TextField() # Définit un champ de texte sans limite de caractères
-    created_date = models.DateTimeField( # Définit le champ en question comme une date ou une heure
+    author = models.ForeignKey('auth.User') 
+    title = models.CharField(max_length=200) 
+    text = models.TextField() 
+    created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
