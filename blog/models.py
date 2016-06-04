7139494b110ccc1664ed0c612e6f6c8df591cc94
MNG <my.ni.jp@gmail.com>
2016-06-04 15:22:53 +0200

    modification console

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 from django.utils import timezone # Permet d'importer des morceaux d'autres fichiers
 
-class Post(models.Model): # Permet de définir le modèle. C'est un objet. "class" indique la définition de l'objet, "Post" est le nom du modèle, "models.Model" siginifie que c'est un modèle Django
+class Post(models.Model):
     author = models.ForeignKey('auth.User') # Lien vers un autre modèle
     title = models.CharField(max_length=200) # Définit un champ de texte avec ici un nombre max de caractères (200)
     text = models.TextField() # Définit un champ de texte sans limite de caractères
